80b22b9da5b4e114c9831ca139bbd9f737270fca
muoi do <dovanmuoi198@gmail.com>
2020-11-28 17:15:43 +0700

    tao database bang migration

---
+++ SolutionShop.Data/EF/SolutionShopdbcontextfactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SolutionShop.Data.EF
+{//phuong thuc ket noi sqlserver de update tu migration
+   public class SolutionShopdbcontextfactory : IDesignTimeDbContextFactory<Shopdbcontext>
+    {
+        public Shopdbcontext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("SolutionShopDatabase");
+            var optionsBuilder = new DbContextOptionsBuilder<Shopdbcontext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new Shopdbcontext(optionsBuilder.Options);
+        }
+    }
+}
