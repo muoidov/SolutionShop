e4126fc1d5633d194f596d4cba187ed01e1fbe1a
muoi do <dovanmuoi198@gmail.com>
2020-11-29 20:13:56 +0700

    them thuoc tinh cho order config nhung ko the add

--- SolutionShop.Data/Configurations/Orderconfiguration.cs
+++ SolutionShop.Data/Configurations/Orderconfiguration.cs
@@ -14,7 +14,21 @@ namespace SolutionShop.Data.Configurations
             builder.ToTable("Orders");
             builder.HasKey(t => t.Id);
             builder.Property(x => x.ShipEmail).IsRequired().IsUnicode(false);
+            builder.Property(x => x.OrderDate);
+
+            builder.Property(x => x.ShipEmail).IsRequired().IsUnicode(false).HasMaxLength(50);
+
+            builder.Property(x => x.ShipAddress).IsRequired().HasMaxLength(200);
+
+
+            builder.Property(x => x.ShipName).IsRequired().HasMaxLength(200);
+
+
+            builder.Property(x => x.ShipPhoneNumber).IsRequired().HasMaxLength(200);
+
             builder.HasOne(x => x.AppUser).WithMany(x => x.Orders).HasForeignKey(x => x.UserId);
+
+
         }
     }
 }
