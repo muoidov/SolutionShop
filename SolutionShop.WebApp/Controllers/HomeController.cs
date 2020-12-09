2504522c80bf57689f52d1378b9e1ed05a1e8088
muoi do <dovanmuoi198@gmail.com>
2020-12-09 11:50:05 +0700

    multilple language cho webapp

--- SolutionShop.WebApp/Controllers/HomeController.cs
+++ SolutionShop.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LazZiya.ExpressLocalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SolutionShop.WebApp.Models;
 using System;
@@ -11,15 +14,19 @@ namespace SolutionShop.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly ISharedCultureLocalizer _loc;
+
+        public HomeController(ILogger<HomeController> logger, ISharedCultureLocalizer loc)
         {
             _logger = logger;
+            _loc = loc;
         }
 
         public IActionResult Index()
         {
+            var msg = _loc.GetLocalizedString("Vietnamese");
             return View();
         }
 
@@ -32,6 +39,16 @@ namespace SolutionShop.WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult SetCultureCookie(string cltr, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cltr)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
--- SolutionShop.WebApp/Startup.cs
+++ SolutionShop.WebApp/Startup.cs
@@ -1,11 +1,15 @@
+using LazZiya.ExpressLocalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SolutionShop.WebApp.LocalizationResources;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,8 +27,23 @@ namespace SolutionShop.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-        }
+            var cultures = new[]
+              {
+                new CultureInfo("en"),
+                new CultureInfo("vi"),
+            };
+            services.AddControllersWithViews().AddExpressLocalization<ExpressLocalizationResource, ViewLocalizationResource>(ops =>
+            {
+
+                ops.UseAllCultureProviders = false;
+                ops.ResourcesPath = "LocalizationResources";
+                ops.RequestLocalizationOptions = o =>
+                {
+                    o.SupportedCultures = cultures;
+                    o.SupportedUICultures = cultures;
+                    o.DefaultRequestCulture = new RequestCulture("vi");
+                };
+            }); }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -45,12 +64,12 @@ namespace SolutionShop.WebApp
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseRequestLocalization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{culture=vi}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
